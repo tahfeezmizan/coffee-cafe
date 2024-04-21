c693bafa0e795fdbbdd2ccf1584055dee3a49b9c
tahfeezmizan <mizanahmed5316@gmail.com>
2024-04-22 00:58:09 +0600

    create add coffee form for add data on mongodb

--- src/components/AddCoffee/AddCoffee.jsx
+++ src/components/AddCoffee/AddCoffee.jsx
@@ -1,13 +1,68 @@
 
 const AddCoffee = () => {
+
+    const handleAddCoffee = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const newCoffee = { name, chef, supplier, taste, category, details, photo }
+        console.log(newCoffee);
+
+    }
+
     return (
         <div className="w-8/12 mx-auto">
-            <div className="text-center bg-slate-100 rounded-lg">
-                <h1>Add New Coffee</h1>
-                <p className="">It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
-               
-                <form >
-                    <input type="text" name="name" id="" />
+            <div className="bg-slate-100 rounded-lg p-8">
+                <h1 className="text-center pb-4">Add New Coffee</h1>
+                {/* <p className="mb-5 text-center">It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p> */}
+
+                <form onSubmit={handleAddCoffee}>
+                    <div className="pb-5">
+                        <div className="flex gap-10">
+                            <div className="flex-1">
+                                <label htmlFor="" className="">Name</label> <br />
+                                <input type="text" name="name" className="w-full" id="" />
+                            </div>
+                            <div className="flex-1">
+                                <label htmlFor="" className="">Chef</label> <br />
+                                <input type="text" name="chef" className="w-full" id="" />
+                            </div>
+                        </div>
+                        <div className="flex gap-10">
+                            <div className="flex-1">
+                                <label htmlFor="" className="">Supplier</label> <br />
+                                <input type="text" name="supplier" className="w-full" id="" />
+                            </div>
+                            <div className="flex-1">
+                                <label htmlFor="" className="">Taste</label> <br />
+                                <input type="text" name="taste" className="w-full" id="" />
+                            </div>
+                        </div>
+                        <div className="flex gap-10">
+                            <div className="flex-1">
+                                <label htmlFor="" className="">Category</label> <br />
+                                <input type="text" name="category" className="w-full" id="" />
+                            </div>
+                            <div className="flex-1">
+                                <label htmlFor="" className="">Details</label> <br />
+                                <input type="text" name="details" className="w-full" id="" />
+                            </div>
+                        </div>
+
+                        <div className="">
+                            <label htmlFor="">Photo</label> <br />
+                            <input type="text" name="photo" className="w-full" id="" />
+                        </div>
+                    </div>
+
+                    <input type="submit" value="add coffee" className="btn border border-gray-400 w-full bg-gray-400" />
                 </form>
             </div>
         </div>
